9c2cccf12c0bcb1adb7f5503e0d231d27fdc8822
sergiop45 <sergiopsantos33@gmail.com>
2022-07-04 02:15:17 -0300

    pagina

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
 import './App.css';
+import {Component} from 'react';
+import './css/style.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+
+      this.state = {
+        nutri: []
+      };
+  }
+
+  componentDidMount() {
+    let url = "https://sujeitoprogramador.com/rn-api/?api=posts";
+    fetch(url)
+    .then((r) => r.json())
+    .then((json) => {
+      let state = this.state;
+      state.nutri = json;
+      this.setState(state);
+    })
+  }
+
+  render() {
+    return (
+      <div className="container">
+
+        <header>
+          Posts Nutrição
+        </header>
+      
+        {this.state.nutri.map((item) => {
+          return (
+              <article className='post' key={item.id}>
+                <strong className='titulo'>{item.titulo}</strong>
+                <img src={item.capa} className="capa" />
+                <p className='subtitulo'>{item.subtitulo}</p>
+                <a href='#' className='botao'>Acessar</a>
+              </article>
+              
+          );
+          
+        })}
+      </div>
+    );
+  }
+  
 }
 
 export default App;
